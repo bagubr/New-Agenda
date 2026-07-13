86cf803224f6c331d68625cdcd5a6d704e37d1b6
distaru <service.distaru@gmail.com>
2026-07-13 17:12:40 +0700

    update from server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+	\Illuminate\Support\Facades\URL::forceScheme('https');
+        URL::forceRootUrl(Config::get('app.url'));
     }
 }
