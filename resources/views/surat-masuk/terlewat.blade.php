b779599f0dbec571bc8e2be83c93e1e1f05a4813
Bagu <bagubragowo3@gmail.com>
2026-07-15 15:26:44 +0700

    fix size upload

--- resources/views/surat-masuk/terlewat.blade.php
+++ resources/views/surat-masuk/terlewat.blade.php
@@ -749,7 +749,7 @@
     var totalFileSize = 0;
 
     var myDropzone = new Dropzone('#myDropzone', {
-        maxFilesize: 2,
+        maxFilesize: 5,
         acceptedFiles: '.pdf,.doc,.docx,.xls,.xlsx,.jpg,.jpeg,.png,.gif',
         addRemoveLinks: true,
         paramName: 'file',
